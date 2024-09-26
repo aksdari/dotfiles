04e4f4b9dd13a67568ce30cd821289f9203ab0aa
Adham Abu Dari <aksdari@gmail.com>
2024-09-25 22:28:05 -0700

    updating formatting settings

---
+++ nvim/lua/plugins/formatting.lua
@@ -0,0 +1,17 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    format_on_save = {
+      -- These options will be passed to conform.format()
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
+    formatters_by_ft = {
+      lua = { "stylua" }, -- Lua formatter
+      python = { "black", "isort" }, -- Python formatters
+      javascript = { "prettier" }, -- JS formatter
+      sh = { "shfmt" },
+      -- Add more formatters by language here
+    },
+  },
+}
--- setup.sh
+++ setup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-# ln -sf ~/dotfiles/.vimrc ~/.vimrc
-# ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/github/dotfiles/nvim ~/.config/nvim
+
+BrewInstall() {
+  brew install neovim lazygit npm go
+}
+
+OtherInstalls() {
+  # install shfmt
+  go install mvdan.cc/sh/v3/cmd/shfmt@latest
+}
+
+CreateSymlinks() {
+  ln -sf ~/github/dotfiles/nvim ~/.config/nvim
+  rm -rf ~/.zshrc
+  ln -sf ~/github/dotfiles/.zshrc ~/.zshrc
+}
+
+BrewInstall
+CreateSymlink
 echo "Dotfiles setup complete!"

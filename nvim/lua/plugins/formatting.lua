3627033aa413a76c1637676ce3f6eaab5302e2c1
Adham Abu Dari <aksdari@gmail.com>
2024-09-29 18:41:02 -0700

    commenting formatting module and adding symbols outliners

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -1,12 +1,12 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" }, -- Lua formatter
-      python = { "black", "isort" }, -- Python formatters
-      javascript = { "prettier" }, -- JS formatter
-      sh = { "shfmt" },
-      -- Add more formatters by language here
-    },
-  },
+  --   "stevearc/conform.nvim",
+  --   opts = {
+  --     formatters_by_ft = {
+  --       lua = { "stylua" }, -- Lua formatter
+  --       python = { "black", "isort" }, -- Python formatters
+  --       javascript = { "prettier" }, -- JS formatter
+  --       sh = { "shfmt" },
+  --       -- Add more formatters by language here
+  --     },
+  --   },
 }

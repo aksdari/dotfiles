dbf6240ea3e36c22a5b91ba5f5fd099a96bbb9b9
Adham Abu Dari <aksdari@gmail.com>
2024-09-30 15:34:59 -0700

    adding git signs plugin config

---
+++ nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 300,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+        use_focus = true,
+      },
+      current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+      sign_priority = 6,
+      update_debounce = 100,
+      status_formatter = nil, -- Use default
+      max_file_length = 40000, -- Disable if file is longer than this (in lines)
+    },
+  },
+}

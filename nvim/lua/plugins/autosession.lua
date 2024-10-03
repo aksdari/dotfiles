e96e0566d30501c4d5bb964ec4906caffe9ba15a
Adham Abu Dari <aksdari@gmail.com>
2024-10-03 08:02:24 -0700

    use autsession default config

--- nvim/lua/plugins/autosession.lua
+++ nvim/lua/plugins/autosession.lua
@@ -7,11 +7,7 @@ return {
     ---@module "auto-session"
     ---@type AutoSession.Config
     opts = {
-      suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-      log_level = "info",
-      auto_session_enabled = true,
-      auto_save_enabled = true,
-      auto_restore_enabled = true,
+      suppressed_dirs = { "~/", "~/Downloads", "/" },
     },
   },
 }

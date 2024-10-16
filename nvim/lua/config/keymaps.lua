5ba6a225e1ca9adcc612842ac9fa1ce42985788a
Adham Abu Dari <aksdari@gmail.com>
2024-10-16 11:47:40 -0700

    <C-k> to show LSP signature help

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local opts = { noremap = true, silent = true }
+
+-- Use <C-k> to show LSP Signature help
+vim.keymap.set({ "n", "i" }, "<C-k>", vim.lsp.buf.signature_help, opts)
